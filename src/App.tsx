1471f4086f0ac3ab811c3990f7ddf728caa50dc6
Karan Ankush Jagtap <karan.10734561@ltimindtree.com>
2024-01-05 20:34:26 +0530

    init: project basics

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import '@mantine/core/styles.css';
+import '@mantine/carousel/styles.css';
+import '@mantine/notifications/styles.css';
 import { MantineProvider } from '@mantine/core';
+import { Notifications } from '@mantine/notifications';
 import { Router } from './Router';
 import { theme } from './theme';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 
 export default function App() {
   return (
     <MantineProvider theme={theme}>
-      <Router />
+      <Provider store={store}>
+        <Notifications position="top-right" />
+        <Router />
+      </Provider>
     </MantineProvider>
   );
 }
--- src/Router.tsx
+++ src/Router.tsx
@@ -1,10 +1,36 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { HomePage } from './pages/Home.page';
+import { HomePage } from './pages/home/Home.page';
+import { AuthPage } from './pages/auth/Auth.page';
+import { CreatePostPage } from './pages/createPost/CreatePost.page';
+import RequireAuth from './components/requireAuth/RequireAuth';
+import { MyPostsPage } from './pages/myPosts/MyPosts.page';
+import { PostDetailsPage } from './sections/postDetails/PostDetails.page';
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <HomePage />,
+    path: '/auth',
+    element: <AuthPage />,
+  },
+  {
+    element: <RequireAuth />,
+    children: [
+      {
+        path: '/',
+        element: <HomePage />,
+      },
+      {
+        path: '/myPosts',
+        element: <MyPostsPage />,
+      },
+      {
+        path: '/createPost',
+        element: <CreatePostPage />,
+      },
+      {
+        path: '/post/:id',
+        element: <PostDetailsPage />,
+      },
+    ],
   },
 ]);
 
